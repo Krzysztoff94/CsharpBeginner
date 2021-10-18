685c5b65be94cf598682f679964fc492a859bb95
Krzysztoff94 <56690405+Krzysztoff94@users.noreply.github.com>
2021-10-18 21:54:29 +0200

    Add files via upload

---
+++ EuclideanAlgorithm/EuclideanAlgorithm/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EuclideanAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First number ");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second number ");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+            bool isEquels = false;
+            while (!isEquels)
+                if (numberOne > numberTwo)
+                {
+                    int subtraction = numberOne - numberTwo;
+                    numberOne = subtraction;
+                }
+
+                else if (numberOne < numberTwo)
+                {
+                    int subtraction2 = numberTwo - numberOne;
+                    numberTwo = subtraction2;
+                }
+
+                else
+                {
+                    isEquels = true;
+                    Console.WriteLine($"The greatest common divisor for the given numbers is: {numberOne}");
+                }
+
+        }
+    }
+}
